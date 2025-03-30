18bdd23d9fa8bfb632dc3af78f77b02668e5fce9
Elan <a932327192@gmail.com>
2025-03-30 23:52:43 +0000

    create database

--- migrations/finished/createdb.sql
+++ migrations/finished/createdb.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE subscriptions;
+CREATE DATABASE IF NOT EXISTS subscriptions;
+USE subscriptions;
+
+CREATE TABLE IF NOT EXISTS subscribers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(255) NOT NULL
+);
